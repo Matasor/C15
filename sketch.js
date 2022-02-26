eef1580c154e57bb55f937fde62327c3b4fdb2a9
Matasor <96345204+Matasor@users.noreply.github.com>
2022-02-26 11:55:27 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -93,15 +93,15 @@ function draw() {
         // jewelryG.destroyEach;
         // swordGroup.destroyEach;
 
-         cashG.destroy();
-         diamondsG.destroy();
-         jewelryG.destroy();
-         swordGroup.destroy();
+        // cashG.destroy();
+        // diamondsG.destroy();
+        // jewelryG.destroy();
+        // swordGroup.destroy();
         
-        // cashG.destroyEach();
-        // diamondsG.destroyEach();
-        // jewelryG.destroyEach();
-        // swordGroup.destroyEach();
+         cashG.destroyEach();
+         diamondsG.destroyEach();
+         jewelryG.destroyEach();
+         swordGroup.destroyEach();
         
         // cashGdestroyEach();
         // diamondsGdestroyEach();
